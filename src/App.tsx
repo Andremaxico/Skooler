a9f44d2c3c62aee00a93b816cd1a30cc3cbe2e3f
Andremaxico <bestthebeschan@gmail.com>
2022-09-12 10:48:15 +0200

    start making chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useContext, useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import 'antd/dist/antd.css'; 
+import Chat from './components/Chat';
+import { UserType } from './utils/types';
+import { Content, Footer } from 'antd/lib/layout/layout';
 
-function App() {
+
+const App = () => {
+  const [accountData, setAccountData] = useState<null | UserType>(null);
+  // const routesData = [
+  //   {
+  //     element: <Chat />, to: '/chat'
+  //   },
+  //   {
+  //     element: <Login />, to: '/login'
+  //   }
+  // ];
+
+  // const routesList = routesData.map((data, i) => {
+  //   return <Route element={data.element} to='/chat'/>
+  // });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header accountData={accountData}/>
+      <Content style={{ padding: '0 50px', flex: '1 1 auto', display: 'flex' }}>
+        <div className="site-layout-content" style={{flex: '1 1 auto'}}>
+          <Routes>
+            <Route path='/login' element={<Login setAccountData={setAccountData} />}/>
+            <Route path='/chat' element={<Chat />}/>
+          </Routes>
+        </div>
+      </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </>
   );
 }
 
-export default App;
+const AppContainer = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+export default AppContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { getAuth, Auth } from 'firebase/auth';
+
+import { initializeApp } from "firebase/app";
+import { Analytics, getAnalytics } from "firebase/analytics";
+import { Firestore, getFirestore } from 'firebase/firestore';
+import { getMessaging } from "firebase/messaging";
+import { Database, getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMkGjC1EwkYTerwkrYzROPdmBDQZsoE7Q",
+  authDomain: "real-time-chat-test-ece84.firebaseapp.com",
+  projectId: "real-time-chat-test-ece84",
+  storageBucket: "real-time-chat-test-ece84.appspot.com",
+  messagingSenderId: "839183074345",
+  appId: "1:839183074345:web:dc7ec68d14d4eb7f3b0838",
+  measurementId: "G-L6Z13LL2ZV",
+  databaseURL: "https://real-time-chat-test-ece84-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth  = getAuth(app);
+const firestore = getFirestore();
+const messaging = getMessaging(app);
+const database = getDatabase(app);
+
+export const FirebaseContext = createContext({
+  auth: null,
+  analytics: null,
+  firestore: null,
+  database: null,
+} as {
+  auth: Auth | null,
+  analytics: Analytics | null,
+  firestore: Firestore | null,
+  database: Database | null,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={{
+      analytics,
+      auth,
+      firestore,
+      database
+  }}>
     <App />
-  </React.StrictMode>
+  </FirebaseContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
