e0099f796c59afb84b9db25aa9c6ea41c388a2bf
Andriy <andremaxico321@gmail.com>
2022-12-24 12:42:06 +0300

    posts addin

--- src/App.tsx
+++ src/App.tsx
@@ -35,20 +35,88 @@ import Stream from './components/Stream';
 import { NewPost } from './components/NewPost';
 import { Post } from './components/Post';
 
+import { deepmerge } from '@mui/utils';
+import {
+  useColorScheme,
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendMuiTheme,
+} from '@mui/material/styles';
+import { extendTheme as extendJoyTheme } from '@mui/joy/styles';
+import { blue, grey } from '@mui/material/colors';
+
 //mui theme
-const theme = createTheme({
+const muiTheme = createTheme({
   palette: {
     primary: {
       main: '#61764B',
-
+      dark: '#4e5e3c',
+      light: '#81916f',
     },
     secondary: {
       main: '#9BA17B',
     },
     
   },
-})
+});
 
+// Note: you can't put `joyTheme` inside Material UI's `extendMuiTheme(joyTheme)`
+// because some of the values in the Joy UI theme refers to CSS variables and
+// not raw colors.
+
+//joyUI theme
+const joyTheme = extendJoyTheme({
+  // This is required to point to `var(--mui-*)` because we are using
+  // `CssVarsProvider` from Material UI.
+  cssVarPrefix: 'mui',
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          ...blue,
+          solidColor: 'var(--mui-palette-primary-contrastText)',
+          solidBg: 'var(--mui-palette-primary-main)',
+          solidHoverBg: 'var(--mui-palette-primary-dark)',
+          plainColor: 'var(--mui-palette-primary-main)',
+          plainHoverBg:
+            'rgba(var(--mui-palette-primary-mainChannel) / var(--mui-palette-action-hoverOpacity))',
+          plainActiveBg: 'rgba(var(--mui-palette-primary-mainChannel) / 0.3)',
+          outlinedBorder: 'rgba(var(--mui-palette-primary-mainChannel) / 0.5)',
+          outlinedColor: 'var(--mui-palette-primary-main)',
+          outlinedHoverBg:
+            'rgba(var(--mui-palette-primary-mainChannel) / var(--mui-palette-action-hoverOpacity))',
+          outlinedHoverBorder: 'var(--mui-palette-primary-main)',
+          outlinedActiveBg: 'rgba(var(--mui-palette-primary-mainChannel) / 0.3)',
+        },
+        neutral: {
+          ...grey,
+        },
+        // Do the same for the `danger`, `info`, `success`, and `warning` palettes,
+        divider: 'var(--mui-palette-divider)',
+        text: {
+          tertiary: 'rgba(0 0 0 / 0.56)',
+        },
+
+      },
+    },
+    // Do the same for dark mode
+    // dark: { ... }
+  },
+  fontFamily: {
+    display: '"Roboto","Helvetica","Arial",sans-serif',
+    body: '"Roboto","Helvetica","Arial",sans-serif',
+  },
+  shadow: {
+    xs: `var(--mui-shadowRing), ${muiTheme.shadows[1]}`,
+    sm: `var(--mui-shadowRing), ${muiTheme.shadows[2]}`,
+    md: `var(--mui-shadowRing), ${muiTheme.shadows[4]}`,
+    lg: `var(--mui-shadowRing), ${muiTheme.shadows[8]}`,
+    xl: `var(--mui-shadowRing), ${muiTheme.shadows[12]}`,
+  },
+});
+
+// You can use your own `deepmerge` function.
+// muiTheme will deeply merge to joyTheme.
+const theme = deepmerge(joyTheme, muiTheme);
 
 const App = () => {
   const { auth } = useContext(FirebaseContext);
@@ -143,9 +211,9 @@ const AppContainer = () => {
   return (
     <HashRouter>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
+        <CssVarsProvider theme={theme}>
           <App />
-        </ThemeProvider>
+        </CssVarsProvider>
       </Provider>
     </HashRouter>
   )
